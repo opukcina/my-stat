5da654c47001b596012fbc80ebfbde37e457f2f3
opukcina <nazrulihsan2004@gmail.com>
2024-09-10 20:07:38 +0700

    Add files via upload

---
+++ pm.java
@@ -0,0 +1,18 @@
+package PL;
+
+public class pm {
+    public static void main(String[] args) {
+        String nama = "Nazrul Ihsan";
+
+        String nim = "12350113165";
+
+        String hobi = "Membaca, main game, nonton drama";
+
+        String email = "nazrulihsan2004@gmail.com";
+
+        System.out.println("Nama \t: " + nama);
+        System.out.println("Nim \t: " + nim);
+        System.out.println("Hobi \t: " + hobi);
+        System.out.println("Email \t: " + email);
+    }
+}
